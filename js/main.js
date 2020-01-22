8776b2e0e6c58913610feb6c349ff0f9d58a5264
Felippex <55957871+Felippex@users.noreply.github.com>
2020-01-22 23:00:17 +0100

    add control number algorythm

--- js/main.js
+++ js/main.js
@@ -25,4 +25,46 @@ btn.addEventListener('click', function () {
   let femaleNumber = Math.floor(Math.random() * 5) * 2;
   let maleNumber = Math.floor((Math.random() * 5)) * 2 + 1;
 
+
+  let peselWihtoutControlNumber;
+
+  if (document.querySelector('.woman').checked) {
+    peselWithoutControlNumber = peselYear + peselMonth + peselDay + orderNumber + femaleNumber;
+    console.log(peselWithoutControlNumber);
+
+
+
+  } else {
+    peselWithoutControlNumber = peselYear + peselMonth + peselDay + orderNumber + maleNumber;
+    console.log(peselWithoutControlNumber);
+  }
+
+  let numberArray = peselWithoutControlNumber.toString().split("")
+  let factors = [1, 3, 7, 9, 1, 3, 7, 9, 1, 3];
+  let numArr = 0;
+  let sumArr = 0;
+
+  for (let i = 0; i < 10; i++) {
+    numArr = numberArray[i] * factors[i];
+
+    if (numArr < 10) {
+      sumArr += numArr;
+    } else {
+      sumArr += parseInt(numArr.toString().charAt(1), 10)
+
+    }
+
+    let lastDigit;
+    if (sumArr.toString().charAt(1) == 0) {
+      lastDigit = 0;
+    } else {
+      lastDigit = 10 - parseInt(sumArr.toString().charAt(1), 10);
+    }
+
+    let pesel = peselWithoutControlNumber + lastDigit;
+
+    strongPESEL.innerHTML = pesel;
+
+
+  }
 })
